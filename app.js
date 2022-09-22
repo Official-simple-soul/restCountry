fdb2d864e5518faf9711d8bbd406cd6c24ec88c9
official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-09-22 15:14:40 +0100

    new

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let changeBtn = document.querySelector('.changebtn');
 let changeBody = document.querySelector('.body')
 
+// switch mode
+const switchMode = () => {
     changeBtn.addEventListener('click', function () {
         changeBody.classList.toggle('change');
 
@@ -12,13 +14,20 @@ let changeBody = document.querySelector('.body')
             changeBtn.innerHTML = 'Light Mode';
         }
     }
-    );
+);
+}
+switchMode()
 
-fetch('https://restcountries.com/v2/all?')
+
+// get data
+function getData() {
+    
+    fetch('https://restcountries.com/v2/all?')
     .then(response => response.json())
     .then(data => {
         console.log(data);
         let countries = data.map(country => {
+            let alpha = country.callingCodes
             return `<div class="col-md-4 mb-5">
             <div class="card border-0 shadow bg-transparent">
                 <img src="${country.flag}" alt="" class="image">
@@ -26,7 +35,7 @@ fetch('https://restcountries.com/v2/all?')
                     <h5 class="card-title">${country.name}</h5>
                     <p class="card-text"><b>Capital:</b> ${country.capital}</p>
                     <p class="card-text"><b>Region:</b> ${country.region}</p>
-                    <a href="#" class="btn btn-primary view-more" onclick="clickMe(${country.name})">Read More</a>
+                    <button class="btn btn-primary view-more" onclick="clickMe(${alpha})">Read More</button>
                 </div>
             </div>
         </div>`;
@@ -34,13 +43,12 @@ fetch('https://restcountries.com/v2/all?')
         document.querySelector('.row').innerHTML = countries;
     }
     ).catch(error => console.log(error));
+}
+getData()
 
 
-//     // search for country
-
-  let search = document.querySelector('.search');
-
-  search.addEventListener('submit', (e) => {
+// search for country
+  let search = document.querySelector('.search').addEventListener('submit', (e) => {
     e.preventDefault();
     let searchValue = document.querySelector('.search-input').value;
     fetch(`https://restcountries.com/v2/name/${searchValue}`)
@@ -56,7 +64,7 @@ fetch('https://restcountries.com/v2/all?')
                         <p class="card-text"><b>Capital:</b> ${country.capital}</p>
                         <p class="card-text"><b>Population:</b> ${country.population}</p>
                         <p class="card-text"><b>Region:</b> ${country.region}</p>
-                        <a href="#" class="btn btn-primary view-more" onclick="clickMe(${country.name})">Read More</a>
+                        <a href="#" class="btn btn-primary view-more" onclick="clickMe()">Read More</a>
                     </div>
                 </div>
             </div>`;
@@ -68,17 +76,12 @@ fetch('https://restcountries.com/v2/all?')
     )
 
 // reload page
-let home = document.querySelector('.home');
-home.addEventListener('click', () => {
-    location.reload();
-}
-)
+let home = document.querySelector('.home').addEventListener('click', () => location.reload());
 
 // filter by region
-let africa = document.querySelectorAll('.region');
-
-for (let i = 0; i < africa.length; i++) {
-    africa[i].addEventListener('click', (e) => {
+let regions = document.querySelectorAll('.region');
+for (let i = 0; i < regions.length; i++) {
+    regions[i].addEventListener('click', (e) => {
         e.preventDefault();
         let region = e.target.innerText;
         fetch(`https://restcountries.com/v2/region/${region}`)
@@ -107,44 +110,13 @@ for (let i = 0; i < africa.length; i++) {
     )
 }
 
-
 // click to see more about country details on a new html page
-
-function clickMe(name) {
-    fetch(`https://restcountries.com/v2/all/${name}`)
+function clickMe(alpha) {
+    fetch(`https://restcountries.com/v2/callingcode/${alpha}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         localStorage.setItem("viewCountry", JSON.stringify(data));
         window.location.href = "view.html";
-        // console.log(data)
       });
   }
-
-
-
-
-
-// async function getCountries() {
-//     let response = await fetch('https://restcountries.com/v2/all');
-//     let data = await response.json();
-//     let countries = data.map(country => {
-//         return `<div class="col-md-4 mb-5">
-//         <div class="card border-0 shadow bg-transparent">
-//             <img src="${country.flag}" alt="" class="image">
-//             <div class="card-body">
-//                 <h5 class="card-title">${country.name}</h5>
-//                 <p class="card-text"><b>Capital:</b> ${country.capital}</p>
-//                 <p class="card-text"><b>Region:</b> ${country.region}</p>
-//                 <a href="#" class="btn btn-primary view-more" onclick="clickMe(${country.name})">Read More</a>
-//             </div>
-//         </div>
-//     </div>`;
-//     }).join('');
-//     document.querySelector('.row').innerHTML = countries;
-// }
-// getCountries();
-
-// // search for country
-
-// let search = document.querySelector('.search');
--- country.js
+++ country.js
@@ -1,12 +1,20 @@
 function checkFeed() {
-    let newObject = localStorage.getItem('viewCountry')
-    console.log(newObject);
-    let post = JSON.parse(newObject)
+    let newObject = localStorage.getItem('viewCountry');
+    let post = JSON.parse(newObject);
     console.log(post)
+    let items = post[0];
 
-    document.getElementById('name').innerHTML = post.name
-    document.getElementById('native').innerHTML = post.native
-    document.getElementById('population').innerHTML = post.population
+    document.getElementById('name').innerHTML = items.name;
+    document.getElementById('flag').src = items.flag;
+    document.getElementById('native').innerHTML = 'Native name: ' + " " + items.nativeName;
+    document.getElementById('population').innerHTML = 'Population: ' + " " + items.population;
+    document.getElementById('region').innerHTML = 'Region: ' + " " + items.region;
+    document.getElementById('capital').innerHTML = 'Capital: ' + " " + items.capital;
+    document.getElementById('sub').innerHTML = 'Sub-region: ' + " " + items.subregion;
+    document.getElementById('currency').innerHTML = 'Currency: ' + " " + items.currencies[0].code;
+    document.getElementById('language').innerHTML = 'Language: ' + " " + items.languages[0].name
+    document.getElementById('domain').innerHTML = 'Top Level Domain: ' + " " + items.topLevelDomain;
 }
-
 checkFeed();
+
+
